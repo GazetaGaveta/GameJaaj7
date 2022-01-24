587da5d5dcd6427c128afac4494a54ad71732839
GazetaGaveta <74062534+GazetaGaveta@users.noreply.github.com>
2022-01-24 20:23:47 -0300

    commit extra

--- Scripts/Telas/MovimentoFundo.gd
+++ Scripts/Telas/MovimentoFundo.gd
@@ -20,7 +20,6 @@ func _ready():
 func _process(delta):
 	$Fundo1.rect_position.x += direcao;
 	$Fundo2.rect_position.x += direcao;
-	print(abs($Fundo1.rect_position.x));
 	if abs($Fundo1.rect_position.x) >= 1024:
 		$Fundo1.rect_position.x = 0;
 		$Fundo2.rect_position.x = -direcao*1024;
